e03d42c4f7f2a25c4bac1d86bcab9160694932e5
wmichaelking1 <wmichaelking1@gmail.com>
2011-09-16 12:57:34 -0400

    R wrapper for svmlight_writer.cc

---
+++ R/write.svmlight.R
@@ -0,0 +1,10 @@
+write.svmlight <- function(x, file, ...) {
+
+  if(class(x) != "svmlight")
+    stop("x must be of class svmlight")
+
+  val <- .Call("svmlight_writer", x$data, x$labels, PACKAGE = "RSofia")
+
+  return (val)
+
+}
