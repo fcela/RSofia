05a1acdb022e51543157277091bccab3a6432cac
wmichaelking1 <wmichaelking1@gmail.com>
2011-09-19 22:57:03 -0400

    casts matrix as svmlight object

---
+++ R/as.svmlight.R
@@ -0,0 +1,17 @@
+as.svmlight <- function(formula, data) {
+
+  mf <- model.frame(formula, data)
+                  
+  y <- mf[,1]
+                          
+  no_bias_term <- ifelse(attr(terms(formula, data=data), "intercept")==0, TRUE, FALSE)
+  
+  mm <- model.matrix(formula, mf) 
+
+  x <- mm[, ifelse(no_bias_term, 1, 2):ncol(mm)]
+                 
+  res <- structure(list(labels = y, data = x), class = "svmlight")
+
+  return (res) 
+
+}
