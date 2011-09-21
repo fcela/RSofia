0b5f0ee33dd28cafb3eafedb4ce0860160258691
wmichaelking1 <wmichaelking1@gmail.com>
2011-09-20 20:30:19 -0400

    further documentation

--- R/predict.sofia.R
+++ R/predict.sofia.R
@@ -1,17 +1,17 @@
 ##############################################################
 # object          : sofia object
-# data            : data to test, this is tricky with matrices
+# data            : data to test, of class svmlight
 # prediction_type : "logistic" or "linear"
 # no_bias_term    : logical "FALSE" by default
 ##############################################################
 
-predict.sofia <- function(object, data, prediction_type, ...) {
+predict.sofia <- function(object, newdata, prediction_type, ...) {
 
   sofia_facade <- new(RSofiaFacade)
 
   x <- sofia_facade$predict(
          object$weights,
-         data,
+         newdata$data,
          object$par$no_bias_term,
          prediction_type
        )  
--- inst/Bash/sofia_test.sh
+++ inst/Bash/sofia_test.sh
@@ -7,11 +7,19 @@ function fit_model {
 ${5}/sofia-ml --learner_type $1 \
            --loop_type $2 \
            --lambda 0.1 \
+           --eta_type pegasos \
            --iterations 100000 \
+           --random_seed 1 \
            --dimensionality 5 \
            --training_file $3 \
-           --model_out $4
-
+           --model_out $4 \
+           --rank_step_probability 0.5 \
+           --passive_aggressive_c 10000000.0 \
+           --passive_aggressive_lambda 0 \
+           --perceptron_margin_size 1.0 \
+           --hash_mask_bits 0
+###--training_objective \ not set
+###--no_bias_term = 0 
 }
 
 LEARNER_TYPE=( "pegasos" "sgd-svm" "passive-aggressive" "margin-perceptron" "romma" "logreg-pegasos" )
