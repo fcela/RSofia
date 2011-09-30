88949b55852c31a24387283b236ee59d4c9b546e
wmichaelking1 <wmichaelking1@gmail.com>
2011-09-30 18:42:20 -0400

    added (working) unit tests for predict, but stil need to add prediction_type='logistic' as part of thest testing

---
+++ inst/unitTests/runit.predict.sofia.R
@@ -0,0 +1,81 @@
+## ------------------------------------------------------------------------- ##
+## Tests for setConfiguration                                                ##
+##                                                                           ##
+## ------------------------------------------------------------------------- ##
+
+cat("\n\nRUnit test cases for 'RSofia:::sofia' function\n\n")
+
+# This function is executed at the end of the unit test
+.tearDown <- function() {
+  if (exists("track", envir=.GlobalEnv)) {
+    rm(track, envir=.GlobalEnv)
+  }
+}
+
+
+# Now the unit tests, following the naming convention test.XXX
+
+# load test data
+
+data(irismod)
+data(sofia_ml_test)
+
+# tests need not be as comprehensive as in sofia.fit
+
+TOLERANCE <- .00001
+RANDOM_SEED <- 1L
+
+sofia.logreg_pegasos.stochastic.pegasos.nobias1 <- sofia(Is.Virginica ~ -1 + .
+ , irismod, learner_type = "logreg-pegasos", loop_type = "stochastic"
+ , eta_type = "pegasos", random_seed = RANDOM_SEED)
+
+sofia.logreg_pegasos.stochastic.pegasos.nobias0 <- sofia(Is.Virginica ~ .
+ , irismod, learner_type = "logreg-pegasos", loop_type = "stochastic"
+ , eta_type = "pegasos", random_seed = RANDOM_SEED)
+
+sofia.pegasos.stochastic.pegasos.nobias1 <- sofia(Is.Virginica ~ -1 + .
+ , irismod, learner_type = "pegasos", loop_type = "stochastic"
+ , eta_type = "pegasos", random_seed = RANDOM_SEED)
+
+sofia.pegasos.stochastic.pegasos.nobias0 <- sofia(Is.Virginica ~ .
+ , irismod, learner_type = "pegasos", loop_type = "stochastic"
+ , eta_type = "pegasos", random_seed = RANDOM_SEED)
+
+test.predict.sofia <- function() {
+
+  #w bias
+  checkEqualsNumeric(
+    predict(
+      sofia.pegasos.stochastic.pegasos.nobias1, irismod, prediction_type = "linear"
+    ),
+    sofia_ml_test[["pegasos_stochastic_pegasos_nobias1"]],
+    tolerance = TOLERANCE
+  )
+  
+  checkEqualsNumeric(
+    predict(
+      sofia.pegasos.stochastic.pegasos.nobias0, irismod, prediction_type = "linear"
+    ),
+    sofia_ml_test[["pegasos_stochastic_pegasos_nobias0"]],
+    tolerance = TOLERANCE
+  )
+  
+  checkEqualsNumeric(
+    predict(
+      sofia.logreg_pegasos.stochastic.pegasos.nobias1, irismod, prediction_type = "linear"
+    ),
+    sofia_ml_test[["logreg-pegasos_stochastic_pegasos_nobias1"]],
+    tolerance = TOLERANCE
+  )
+  
+  checkEqualsNumeric(
+    predict(
+      sofia.logreg_pegasos.stochastic.pegasos.nobias0, irismod, prediction_type = "linear"
+    ),
+    sofia_ml_test[["logreg-pegasos_stochastic_pegasos_nobias0"]],
+    tolerance = TOLERANCE
+  )
+    
+}
+
+
